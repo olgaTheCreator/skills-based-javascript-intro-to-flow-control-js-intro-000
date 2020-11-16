9e287b06e8f0db9a9736f6bfaf802adba5de1008
Olga Curylo <curylo.olga@gmail.com>
2020-11-16 12:33:03 +0000

    Automatically backed up by Learn

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -34,10 +34,10 @@ function switchAge(age) {
   case 17:
   case 18:
   case 19:
-  console.log("You are a teenager");
+  return ("You are a teenager");
   break;
   default:
-  console.log("You have an age");
+  return ("You have an age");
 }
 }
 
